0f4e2f26eb8d72babec5502dbcbbddd80f1cf0a3
emilin <164918748+emilinvitoria@users.noreply.github.com>
2024-11-18 14:25:06 -0300

    cartao

---
+++ app.js
@@ -0,0 +1,21 @@
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementsById('container')
+    let cartao = document.createElement('article')
+    cartao.className = ('cartao')
+
+    cartao.innerHTML = <div class= "cartao_conteudo">
+    < h3> progamação</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p> python?</p>
+    </div> 
+    <div class="cartao_conteudo_pergunta">
+    <p> o javascript é uma linguagem de progamação.</p>
+    </div>
+    </div>
+ let respostaEstaVisivel = false
+ function viraCartao() {
+
+ }
+
+
+}
