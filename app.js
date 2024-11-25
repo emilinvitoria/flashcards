a276e67435f55af0b74ffc5d2d54b016c4c43995
emilin <164918748+emilinvitoria@users.noreply.github.com>
2024-11-25 14:04:58 -0300

    cartao

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ function criaCartao(categoria, pergunta, resposta){
     let cartao = document.createElement('article')
     cartao.className = ('cartao')
 
-    cartao.innerHTML = <div class= "cartao_conteudo">
+    cartao.innerHTML = 
+    <div class= "cartao_conteudo">
     < h3> progamação</h3>
     <div class="cartao_conteudo_pergunta">
     <p> python?</p>
@@ -12,10 +13,13 @@ function criaCartao(categoria, pergunta, resposta){
     <p> o javascript é uma linguagem de progamação.</p>
     </div>
     </div>
+
  let respostaEstaVisivel = false
  function viraCartao() {
-
+ respostaEstaVisivível = !respostaEstaVisível
+ cartao.classList.toggle('active',respostaEstaVisível)
  }
+ cartao.eddEventListener('click', viracartao)
 
-
+ container.appendChild(cartao)
 }
